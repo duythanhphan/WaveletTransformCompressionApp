a1a5a3b637a90dc3cfeb34de1549d49fb28f1a39
Michał Kolczyk <kolczykm@gmail.com>
2011-12-31 18:09:06 +0100

    Test wykonania transformaty falkowej na obrazie, zapis obrazka, transformata odwrotna obrazka.

--- main.cpp
+++ main.cpp
@@ -8,12 +8,38 @@
 #include <cstdio>
 
 #include "WaveletCompressor.h"
+#include "WaveletDecompressor.h"
 
 int main(int argc, char* argv[]) {
 
 	WaveletCompressor compressor;
 	compressor.init("input.png", "output");
 	compressor.compress(WaveletCompressor::Haar);
+
+	WaveletDecompressor decompressor;
+	decompressor.init("output", "decompressed.bmp");
+	decompressor.decompress();
+
+	/*
+	Image image;
+	image.load("input.png");
+
+	unsigned int width = FreeImage_GetWidth(image.getDib());
+	unsigned int height = FreeImage_GetHeight(image.getDib());
+	BYTE* bits;
+	BYTE test;
+	unsigned int BytesPerPixel = FreeImage_GetBPP(image.getDib()) / 8;
+
+	for(unsigned int y = 0; y < height; ++y) {
+		bits = FreeImage_GetScanLine(image.getDib(), y);
+		for(unsigned int x = 0; x < width; ++x) {
+			test = bits[FI_RGBA_RED];
+			bits += BytesPerPixel;
+		}
+	}
+
+	printf("test: %d\n", test);
+	*/
 
 	return 0;
 }
