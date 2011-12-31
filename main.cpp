27abe7e7409477b1396823a40274363373d45419
Michał Kolczyk <kolczykm@gmail.com>
2011-12-31 12:20:30 +0100

    Stworzenie projektu aplikacji używającej WAveletTransformCompression

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 31-12-2011
+ *      Author: Michal
+ */
+
+#include <cstdio>
+
+#include "WaveletCompressor.h"
+
+int main(int argc, char* argv) {
+	return 0;
+}
+
+
